1bb3212158482a283abeabbd5d339602fb9c8fff
bowiexue <bowiexue2015@gmail.com>
2026-08-17 15:23:53 -0700

    Update database.js

--- database.js
+++ database.js
@@ -1,30 +1,93 @@
-var maps = {
-  goth: {A:"𝕬",B:"𝕭",C:"𝕮",D:"𝕯",E:"𝕰",F:"𝕱",G:"𝕲",H:"𝕳",I:"𝕴",J:"𝕵",K:"𝕶",L:"𝕷",M:"𝕸",N:"𝕹",O:"𝕺",P:"𝕻",Q:"𝕼",R:"𝕽",S:"𝕾",T:"𝕿",U:"𝖀",V:"𝖁",W:"𝖂",X:"𝖃",Y:"𝖄",Z:"𝖅",a:"𝖆",b:"𝖇",c:"𝖖",d:"𝖉",e:"𝖊",f:"𝖋",g:"𝖌",h:"𝖖",i:"𝖎",j:"𝖏",k:"𝖐",l:"𝖑",m:"𝖒",n:"𝖓",o:"𝖔",p:"𝖕",q:"𝖖",r:"𝖗",s:"𝖘",t:"𝖙",u:"𝖚",v:"𝖛",w:"𝖜",x:"𝖝",y:"𝖞",z:"𝖟"},
-  cute: {A:"卂",B:"乃",C:"匚",D:"刀",E:"乇",F:"下",G:"ム",H:"卄",I:"丨",J:" Kraken",K:"长",L:" tailgate",M:"爪",N:"𠏋",O:"口",P:"尸",Q:" DASHBOARD",R:"尺",S:"丂",T:"丅",U:"ㄩ",V:"ᐪ",W:"山",X:"乂",Y:"ㄚ",Z:"乙",a:"α",b:"в",c:"¢",d:"∂",e:"є",f:"ƒ",g:"g",h:"н",i:"ι",j:"נ",k:"к",l:"ℓ",m:"м",n:"и",o:"σ",p:"ρ",q:"q",r:"я",s:"ѕ",t:"т",u:"υ",v:"ν",w:"ω",x:"χ",y:"у",z:"ｚ"},
-  bubbly: {A:"Ⓐ",B:"Ⓑ",C:"Ⓒ",D:"Ⓓ",E:"Ⓔ",F:"Ⓕ",G:"Ⓖ",H:"Ⓗ",I:"Ⓘ",J:"Ⓙ",K:"Ⓚ",L:"Ⓛ",M:"Ⓜ",N:"Ⓝ",O:"Ⓞ",P:"Ⓟ",Q:"Ⓠ",R:"Ⓡ",S:"Ⓢ",T:"Ⓣ",U:"Ⓤ",V:"Ⓥ",W:"Ⓦ",X:"Ⓧ",Y:"Ⓨ",Z:"Ⓩ",a:"ⓐ",b:"ⓑ",c:"ⓒ",d:"ⓓ",e:"ⓔ",f:"ⓕ",g:"ⓖ",h:"ⓗ",i:"ⓘ",j:"ⓙ",k:"ⓚ",l:"ⓛ",m:"ⓜ",n:"ⓝ",o:"ⓞ",p:"ⓟ",q:"ⓠ",r:"ⓡ",s:"ⓢ",t:"ⓣ",u:"ⓤ",v:"ⓥ",w:"ⓦ",x:"ⓧ",y:"ⓨ",z:"ⓩ"},
-  cursive: {A:"𝒜",B:"ℬ",C:"𝒞",D:"𝒟",E:"ℰ",F:"ℱ",G:"𝒢",H:"ℋ",I:"ℐ",J:"𝒥",K:"𝒦",L:"ℒ",M:"ℳ",N:"𝒩",O:"𝒪",P:"𝒫",Q:"𝒬",R:"ℛ",S:"𝒮",T:"𝒯",U:"𝒰",V:"𝒱",W:"𝒲",X:"𝒳",Y:"𝒴",Z:"𝒵",a:"𝒶",b:"𝒷",c:"𝒸",d:"𝒹",e:"ℯ",f:"𝒻",g:"ℊ",h:"𝒽",i:"𝒾",j:"𝒿",k:"𝓀",l:"𝓁",m:"𝓂",n:"𝓃",o:"ℴ",p:"𝓅",q:"𝓆",r:"𝓇",s:"𝓈",t:"𝓉",u:"𝓊",v:"𝓋",w:"𝓌",x:"𝓍",y:"𝓎",z:"𝓏"},
-  brackets: {A:"【A】",B:"【B】",C:"【C】",D:"【D】",E:"【E】",F:"【F】",G:"【G】",H:"【H】",I:"【I】",J:"【J】",K:"【K】",L:"【L】",M:"【M】",N:"【N】",O:"【O】",P:"【P】",Q:"【Q】",R:"【R】",S:"【S】",T:"【T】",U:"【U】",V:"【V】",W:"【W】",X:"【X】",Y:"【Y】",Z:"【Z】",a:"【a】",b:"【b】",c:"【c】",d:"【d】",e:"【e】",f:"【f】",g:"【g】",h:"【h】",i:"【i】",j:"【j】",k:"【k】",l:"【l】",m:"【m】",n:"【n】",o:"【o】",p:"【p】",q:"【q】",r:"【r】",s:"【s】",t:"【t】",u:"【u】",v:"【v】",w:"【w】",x:"【x】",y:"【y】",z:"【z】"}
+// ================= EXTENDED ECOSYSTEM DATA: PART 1 =================
+var plantPets = {
+  sprout: "🐛", cactus: "🦎", flower: "🦋", orchid: "🐝", rose: "🐞",
+  tulip: "🐌", bamboo: "🐼", bonsai: "🦉", mushroom: "🦔", clover: "🐇",
+  fern: "🐸", maple: "🐿️", palm: "🦜", venus: "🕷️", berry: "🦝"
 };
 
-var plantPets = {
-  sprout: "🐛", cactus: "🦎", flower: "🦋", orchid: "🛸", rose: "🐞", tulip: "🐝", bamboo: "🐼",
-  bonsai: "🦜", mushroom: "🧚", clover: "🐿️", fern: "🐸", maple: "🦉", palm: "🦩", venus: "🪰", berry: "🦔"
+var maps = {
+  f1: {"A":"𝔸","B":"𝔹","C":"ℂ","D":"𝔻","E":"𝔼","F":"𝔽","G":"𝔾","H":"ℍ","I":"𝕀","J":"𝕁","K":"𝕂","L":"𝕃","M":"𝕄","N":"ℕ","O":"𝕆","P":"ℙ","Q":"ℚ","R":"ℝ","S":"𝕊","T":"𝕋","U":"𝕌","V":"𝕍","W":"𝕎","X":"𝕏","Y":"𝕐","Z":"ℤ","a":"𝕒","b":"𝓕","c":"𝕔","d":"𝕕","e":"𝕖","f":"𝕗","g":"𝕘","h":"𝕙","i":"𝕚","j":"𝕛","k":"𝕜","l":"𝕝","m":"𝕞","n":"𝕟","o":"𝕠","p":"𝕡","q":"𝕢","r":"𝕣","s":"𝕤","t":"𝕥","u":"𝕦","v":"𝕧","w":"𝕨","x":"𝕩","y":"𝕪","z":"𝕫"},
+  f2: {"A":"𝔄","B":"𝔅","C":"  ","D":"𝔇","E":"𝔈","F":"𝔉","G":"𝔊","H":"ℌ","I":"ℑ","J":"𝔍","K":"𝔎","L":"𝔏","M":"𝔐","N":"𝔑","O":"𝔒","P":"𝔓","Q":"𝔔","R":"ℜ","S":"𝔖","T":"𝔗","U":"𝔘","V":"𝔙","W":"𝔚","X":"𝔛","Y":"𝔜","Z":"ℨ","a":"𝔞","b":"𝔟","c":"𝔔","d":"𝔡","e":"𝔢","f":"𝔣","g":"𝔤","h":"𝔥","i":"𝔦","j":"𝔧","k":"𝔨","l":"𝔩","m":"𝔪","n":"𝔫","o":"𝔬","p":"𝔭","q":"𝔮","r":"𝔯","s":"𝔰","t":"𝔱","u":"𝔲","v":"𝔳","w":"𝔴","x":"𝔵","y":"𝔶","z":"𝔷"},
+  f3: {"A":"𝓠","B":"𝓑","C":"𝓒","D":"𝓓","E":"𝓔","F":"𝓕","G":"𝓖","H":"𝓗","I":"𝓘","J":"𝓙","K":"𝓚","L":"𝓛","M":"𝓜","N":"𝓝","O":"𝓞","P":"𝓟","Q":"𝓠","R":"𝓡","S":"𝓢","T":"𝓣","U":"𝓤","V":"𝓥","W":"𝓦","X":"𝓿","Y":"加入","Z":"𝓩","a":"𝓪","b":"𝓫","c":"𝓬","d":"𝓭","e":"𝓮","f":"𝓯","g":"𝓰","h":"𝓱","i":"𝓲","j":"𝓳","k":"𝓴","l":"𝓵","m":"𝓶","n":"𝓷","o":"𝓸","p":"𝓹","q":" get","r":"𝓻","s":"𝓼","t":"𝓽","u":"𝓾","v":"𝓿","w":"𝔀","x":"𝔁","y":"𝔂","z":"𝔃"}
 };
 
 var plantData = {
-  sprout: { desc: "Thick green sprouts fix vital nitrogen directly into your farming soil beds.", kaomojis: ["🌱(•◡•)🌱", "🌿(🧱_🧱)", "🌱(🪐_🪐)🌱", "🌱(˘ᵕ˘)🌱"], fonts: [{key:"bubbly", name:"Bubble"},{key:"cursive", name:"Script"},{key:"brackets", name:"Block"}] },
-  cactus: { desc: "Desert structures store moisture reservoirs beneath protective needles.", kaomojis: ["🌵(O_O)🌵", "🌵(•_•)", "🌵(◣_◢)🌵", "🌵(╥﹏╥)🌵"], fonts: [{key:"goth", name:"Spiky"},{key:"brackets", name:"Block"}] },
-  flower: { desc: "Sunflowers dynamically track summer path coordinates to maximize solar intake.", kaomojis: ["🌻(^◡^)", "🌻(🌻_🌻)", "🌻(☀️_☀️)🌻", "🌻(🕶️_🕶️)"], fonts: [{key:"cute", name:"Petal"},{key:"cursive", name:"Script"}] },
-  orchid: { desc: "Exotic aerial roots capture raw atmospheric humidity inside tropical biomes.", kaomojis: ["🌸(◕‿◕)", "🌸(◌_◌)", "🌸(🍧_🍧)🌸", "🌸(💎_💎)"], fonts: [{key:"cute", name:"Mystic"},{key:"bubbly", name:"Bubble"}] },
-  rose: { desc: "Crimson petals release sweet aromatic notes guarded by tiny thorns.", kaomojis: ["🌹(˘‿˘)", "🌹(•_•)", "🌹(🖤_🖤)🌹", "🌹(⚔️_⚔️)"], fonts: [{key:"goth", name:"Goth"},{key:"cursive", name:"Script"}] },
-  tulip: { desc: "Spring bulbs track morning temperature variances to open pristine geometric cups.", kaomojis: ["🌷(ᵔ◡ᵔ)", "🌷(°_°)", "🌷(🎈_🎈)🌷", "🌷(🧁_🧁)"], fonts: [{key:"bubbly", name:"Pop"},{key:"brackets", name:"Block"}] },
-  bamboo: { desc: "Segmented stalks transfer forest water supplies rapidly along giant colonies.", kaomojis: ["🎋(•‿•)", "🎍(⚙️_⚙️)", "🎋(🧘_🧘)🎋", "🎍(🎋_🎋)"], fonts: [{key:"cute", name:"Zen"},{key:"cursive", name:"Script"}] },
-  bonsai: { desc: "Miniature root branches reflect decades of specialized master shaping care.", kaomojis: ["🌳(⛩️_⛩️)", "🌳(˘ᵕ˘)", "🌳(⏳_⏳)🌳", "🌳(👑_👑)"], fonts: [{key:"goth", name:"Ancient"},{key:"brackets", name:"Block"}] },
-  mushroom: { desc: "Fungal spore nodes spread underground network messaging protocols.", kaomojis: ["🍄(🍄_🍄)", "🍄(o_o)", "🍄(✨_✨)🍄", "🍄(🧪_🧪)"], fonts: [{key:"bubbly", name:"Shroom"},{key:"cursive", name:"Script"}] },
-  clover: { desc: "Rare four-leaf variances contain bio-luminescent cellular anomaly structures.", kaomojis: ["🍀(💎_💎)", "🍀(◠‿◠)", "🍀(⭐_⭐)🍀", "🍀(🎰_🎰)"], fonts: [{key:"cute", name:"Lucky"},{key:"bubbly", name:"Bubble"}] },
-  fern: { desc: "Prehistoric shade fronds unfurl complex mathematical fractal branches.", kaomojis: ["🌿(👁️_👁️)", "🌿(•◡•)", "🌿(🦖_🦖)🌿", "🌿(🧭_🧭)"], fonts: [{key:"goth", name:"Jungle"},{key:"brackets", name:"Block"}] },
-  maple: { desc: "Autumn trees convert stored starches into sugary syrup deposits.", kaomojis: ["🍁(🍁_🍁)", "🍁(˘◡˘)", "🍁(☕_☕)🍁", "🍁(🥞_🥞)"], fonts: [{key:"bubbly", name:"Amber"},{key:"cursive", name:"Script"}] },
-  palm: { desc: "Oasis canopy trees weather extreme coastal storm shifts with flexible fibers.", kaomojis: ["🌴(😎)", "🌴(🏝️)", "🌴(🍹_🍹)🌴", "🌴(☀️_☀️)"], fonts: [{key:"cute", name:"Tropic"},{key:"brackets", name:"Block"}] },
-  venus: { desc: "Sensory trigger hairs compute movement limits to trap active bugs.", kaomojis: ["🌱(💥_💥)", "🌱(◣_◢)", "🌱(👹_👹)🌱", "🌱(🩸_🩸)"], fonts: [{key:"goth", name:"Trap"},{key:"cursive", name:"Script"}] },
-  berry: { desc: "Wild thickets produce highly concentrated antioxidant berry nodes.", kaomojis: ["🍓(🍭_🍭)", "🍓(ᵔᵕᵔ)", "🍓(🥧_🥧)🍓", "🍓(🧺_🧺)"], fonts: [{key:"bubbly", name:"Berry"},{key:"brackets", name:"Block"}] }
+  sprout: {
+    desc: "A soft sprout standard seedling.",
+    fonts: [{key:"f1",name:"Double"},{key:"f2",name:"Gothic"},{key:"f3",name:"Script"}],
+    kaomojis: ["🌱","(🌱_🌱)","(•‿•)🌱","(〃‿〃)✿","(˘³˘)🌿","(o^▽^o)","(─‿─)","(✧ω✧)","(◕‿◕)","(シ. .)シ","(っ˘ω˘ς)","( ´▽` )","(っ﹒︠ᴗ﹒︡)っ","(◍•ᴗ•◍)","(〃^▽^〃)","( ˙꒳˙ )"]
+  },
+  cactus: {
+    desc: "A spiked prickly desert dweller.",
+    fonts: [{key:"f1",name:"Double"},{key:"f2",name:"Gothic"}],
+    kaomojis: ["🌵","(🌵_🌵)","(•̀⤙•́)🌵","(⚔️_⚔️)","(ง'̀-'́)ง","(╬皿╬)","(`^´)","(☄️_☄️)","(•̀⤙•́)","(▀̿Ĺ̯▀̿ ̿)","(ง •̀_•́)ง","┌∩┐(◣_◢)","(𓁹_𓁹)","(ಠ_ಠ)","(；′⌒`)","(⁎˃⌊˂⁎)"]
+  },
+  flower: {
+    desc: "A glowing, sun-loving cheerful bloom.",
+    fonts: [{key:"f1",name:"Double"},{key:"f3",name:"Script"}],
+    kaomojis: ["🌻","(🌻_🌻)","(◕‿◕✿)","(✨‿✨)","(✿◠‿◠)","(💖‿💖)","(つ✧ω✧)つ","(⺣◡⺣)♡*","(✿❦‿❦)","(人•͈ᴗ•͈)","(〃▽〃)","(☆▽☆)","(*^ω^*)","(≧◡≦)","(🌸◠‿◠)","(𓆩♡𓆪)"]
+  },
+  orchid: {
+    desc: "An exotic purple orchid bloom.",
+    fonts: [{key:"f2",name:"Gothic"},{key:"f3",name:"Script"}],
+    kaomojis: ["🌸","(🌸_🌸)","(☯‿☯)","(👁️_👁️)","(𓁹‿𓁹)","(✧_✧)","( ° 👁️ °)","(✷‿✷)","(つ✧▽✧)つ","(❦‿❦)","(ꏿ﹏ꏿ)","(❍ᴥ❍ʋ)","(☉_☉)","(⚆_⚆)","( ఠ ͟ʖ ఠ)","(•̪●)"]
+  },
+  rose: {
+    desc: "A deep red classic romantic rose stem.",
+    fonts: [{key:"f1",name:"Double"},{key:"f3",name:"Script"}],
+    kaomojis: ["🌹","(🌹_🌹)","(❤‿❤)","(✿𓁹‿𓁹)","(๑♡⌓♡๑)","(｡♥‿♥｡)","(♡ω♡*)","(✿ ♡‿♡)","(′▽`〃)♡","(づ￣ ³￣)づ","( ́ ∋ ́)","(♥ω♥*)","(๑˘ᵕ˘)","(ღ˘⌣˘ღ)","(◌^▽^◌)","( ´ ▽ ` )ﾉ"]
+  },
+  tulip: {
+    desc: "A gentle pastel colored spring tulip.",
+    fonts: [{key:"f1",name:"Double"},{key:"f2",name:"Gothic"}],
+    kaomojis: ["🌷","(🌷_🌷)","(˘ᵕ˘)✿","(ˊᵕˋ)","(𓂂ᵕ𓂂)","(◍ꂚ◍)","(｡•ㅅ•｡)","(๑•᎑•๑)","( . •́ _ •̀ . )","( ˙▿˙ )","( ˶ˆ꒳ˆ˵ )","( ˊᵕˋ )♡","(„• ֊ •„)","(。U ω U。)","(๑ᵔ⤙ᵔ๑)","(* ´ ` *)"]
+  },
+  bamboo: {
+    desc: "Fast growing segmented tall bamboo shoots.",
+    fonts: [{key:"f2",name:"Gothic"},{key:"f3",name:"Script"}],
+    kaomojis: ["🎍","(🎍_🎍)","(•ᴥ•)","🐼","(￣(oo)￣)","(ˆ 🎞️ ˆ)","(𓃟)","(꜆˶ᵔᵕᵔ˶)꜆","( 𓃠 )","(•⚇•)","(◕ܫ◕)","(✧ܫ✧)","( ´(oo)ˋ )","(ᵔᴥᵔ)","(•̀⤙•́)🐼","(=^･ω･^=)"]
+  },
+  bonsai: {
+    desc: "A miniaturized carefully sculpted ancient tree.",
+    fonts: [{key:"f1",name:"Double"},{key:"f2",name:"Gothic"},{key:"f3",name:"Script"}],
+    kaomojis: ["🌳","(🌳_🌳)","(𓁹_𓁹)","(─‿─)","(🧘)","(˘_˘)","(◡_◡)","(𓆗)","(๑˘ᵕ˘)","( 🧘‍♂️ )","( •_•)","(⇀_⇀)","(ー_ー)","( 一一)","( 𓁺 𓁺 )","(˘◡˘)"]
+  }
 };
+// ================= EXTENDED ECOSYSTEM DATA: PART 2 =================
+Object.assign(plantData, {
+  mushroom: {
+    desc: "A colorful spotted cap forest mushroom.",
+    fonts: [{key:"f1",name:"Double"},{key:"f2",name:"Gothic"}],
+    kaomojis: ["🍄","(🍄_🍄)","(🥴)","(👁️👄👁️)","(🤪)","(✧ω✧)","(★ω★)","(⊙_⊙)","(ʘ‿ʘ)","(ꏿ_ꏿ)","(🤯)","(✧﹃ ✧)","(★﹃ ★)","( 😵‍💫 )","( ಡ_ಡ )","(ʘ╻ʘ)"]
+  },
+  clover: {
+    desc: "A super rare lucky four-leaf clover sprig.",
+    fonts: [{key:"f1",name:"Double"},{key:"f3",name:"Script"}],
+    kaomojis: ["🍀","(🍀_🍀)","(💸_💸)","(🤑)","(🎰)","(✧▽✧)","(💰‿💰)","(👉🖨️)","(🌟‿🌟)","(⭐_⭐)","(🎯)","(💎‿💎)","(🎩)","(  ✨ )","(∩^o^)⊃━☆","(っ🔥‿🔥)っ"]
+  },
+  fern: {
+    desc: "Feathery wild green jungle fern leaves.",
+    fonts: [{key:"f2",name:"Gothic"},{key:"f3",name:"Script"}],
+    kaomojis: ["🌿","(🌿_🌿)","(🐊)","(🦖)","(🦎)","(🌴)","(𓆏)","(𓆗)","(𓆈)","(𓅭)","(🛸)","(🪵)","(𓃰)","(𓄿)","(𓅓)","(𓆲)"]
+  },
+  maple: {
+    desc: "An autumn colored brilliant crimson maple branch.",
+    fonts: [{key:"f1",name:"Double"},{key:"f2",name:"Gothic"}],
+    kaomojis: ["🍁","(🍁_🍁)","(🍂)","(☕)","(🧣)","(˘ᵕ˘)","( 🍵 )","(｡◕‿◕｡)","(っ˘ڡ˘ς)","( ´ρ`)","(●´ω｀●)","( ′～′)","(◌^▽^◌)","( ´∀`)","( 🍁 ‿ 🍁 )","( ☕ _ ☕ )"]
+  },
+  palm: {
+    desc: "A tropical breezy layout ocean coast palm frond.",
+    fonts: [{key:"f1",name:"Double"},{key:"f3",name:"Script"}],
+    kaomojis: ["🌴","(🌴_🌴)","(🕶️)","(🏄)","(🥥)","(🍍)","(🍹)","(𓆉)","(🏖️)","(🦈)","(𓆝)","(𓆟)","(𓟿)","(🌊)","(𓄿)","(☀️)"]
+  },
+  venus: {
+    desc: "A snap-shut carnivorous active fly trap plant.",
+    fonts: [{key:"f2",name:"Gothic"},{key:"f3",name:"Script"}],
+    kaomojis: ["🌱","(👹)","(💀)","(Ψ)","(◣_◢)","(𓆦)","(𓆨)","(𓆣)","(𓆧)","(🔥_🔥)","(╬ﾟ◥益◤ﾟ)","(ಠ益ಠ)","( 𓃓 )","( 𓄿 )","( 𓃠 )","( 💢 )"]
+  },
+  berry: {
+    desc: "A wild vine bursting with juicy forest berries.",
+    fonts: [{key:"f1",name:"Double"},{key:"f2",name:"Gothic"},{key:"f3",name:"Script"}],
+    kaomojis: ["🍓","(🍓_🍓)","(🍇)","(🍒)","(🍰)","(っ˘ڡ˘ς)","(〃^▽^〃)","(๑>◡<๑)","(≧ڡ≦)","(￣▽￣)","(o^ ^o)","(っ﹒︠ᴗ﹒︡)っ","(〃▽〃)","( ˙▿˙ )","( ˶ˆ꒳ˆ˵ )","( 🍓 ‿ 🍓 )"]
+  }
+});
